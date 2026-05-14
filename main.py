394b1c54e8b55e887678fc21338c12209bed6d8b
DeVine-byte <Divine4529@gmail.com>
2026-05-14 11:57:10 +0100

    Refactor CORS and admin setup with environment variables
    Updated CORS settings and admin creation logic to use environment variables.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
 from app.auth import router as auth_router, hash_password
 from app.models import db
 from app.cms import router as cms_router
 
+# LOAD ENV VARIABLES
+load_dotenv()
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "https://broadcast-platform.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -26,11 +34,17 @@ app.include_router(
 # =========================
 @app.on_event("startup")
 def startup():
-    if not db.find_admin("admin"):
-        db.create_admin(
-            username="admin",
-            password=hash_password("admin")
-        )
+
+    ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+
+    # CREATE ADMIN ONLY IF NOT EXISTS
+    if ADMIN_USERNAME and ADMIN_PASSWORD:
+        if not db.find_admin(ADMIN_USERNAME):
+            db.create_admin(
+                username=ADMIN_USERNAME,
+                password=hash_password(ADMIN_PASSWORD)
+            )
 
 # AUTH ROUTES
 app.include_router(
