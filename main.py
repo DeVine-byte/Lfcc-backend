4cf02b4f3e93877a4601203655f798a979833b3a
DeVine-byte <Divine4529@gmail.com>
2026-05-14 17:49:24 +0100

    Add new allowed origin for CORS middleware

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://broadcast-platform.onrender.com/"
+        "https://lfcc-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
